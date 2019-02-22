8c7925560da006fc3cada8e16e07c13c081a7fe0
skd <970335012@qq.com>
2019-02-22 10:35:49 +0800

    提交工程

---
+++ app/src/main/java/com/keding/anrwatchdogdemo/ANRWatchdogTestApplication.java
@@ -0,0 +1,46 @@
+package com.keding.anrwatchdogdemo;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.github.anrwatchdog.ANRError;
+import com.github.anrwatchdog.ANRWatchDog;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * @author: skd
+ * @date 2019/2/22
+ * @Desc ANRWatchdogTestApplication
+ */
+public class ANRWatchdogTestApplication extends Application {
+    ANRWatchDog anrWatchDog = new ANRWatchDog(2000);
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        anrWatchDog.setANRListener(new ANRWatchDog.ANRListener() {
+            @Override
+            public void onAppNotResponding(ANRError error) {
+                Log.e("ANR-Watchdog", "Detected Application Not Responding!");
+
+                // Some tools like ACRA are serializing the exception, so we must make sure the exception serializes correctly
+                try {
+                    new ObjectOutputStream(new ByteArrayOutputStream()).writeObject(error);
+                }
+                catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                Log.i("ANR-Watchdog", "Error was successfully serialized");
+
+                throw error;
+            }
+        });
+
+        anrWatchDog.start();
+    }
+}
